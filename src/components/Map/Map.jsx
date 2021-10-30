96337b7680410ed0c4251b6988d8edfdc6f96da0
James Kim <jamesjkim8806@gmail.com>
2021-10-29 23:52:17 -0700

    changes to map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,21 +7,21 @@ TASKS:
 */
 
 import React, {useState, useEffect} from 'react';
-import hooks from '../../hooks/hooks'
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export default function Map(){
   
-  // hooks.useScriptForMaps();
-
-
-
-  useEffect(() => {
-    L.map('map').setView([51.505, -0.09], 13);
-  }, [])
   return(
-    <div id="map">
-      <h1>Map goes here</h1>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
