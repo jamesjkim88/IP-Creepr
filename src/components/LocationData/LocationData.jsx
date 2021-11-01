d48ddab2c043588e0210664cc0da9413123c35ba
James Kim <jamesjkim8806@gmail.com>
2021-10-31 17:23:51 -0700

    location data comp rendering

--- src/components/LocationData/LocationData.jsx
+++ src/components/LocationData/LocationData.jsx
@@ -1,18 +1,18 @@
 import React, {useState} from 'react';
 
-export default function LocationData({data}){
+export default function LocationData({inputIPData}){
 
   // helper function for location rendering
   function locationRender(){
-    if(!data) return; // empty return if data doesn't exist
+    if(!inputIPData) return; // empty return if data doesn't exist
 
     // render blank strings before api call is made so location is should render nada
-    if(data){
+    if(inputIPData){
       // if city and/or state is null
-      if(data.city === null && data.region_iso_code === null) {
-        return `${data.country} ${data.flag.emoji}`;
+      if(inputIPData.city === null && inputIPData.region_iso_code === null) {
+        return `${inputIPData.country} ${inputIPData.flag.emoji}`;
       }else{
-        return `${data.city}, ${data.region_iso_code}`;
+        return `${inputIPData.city}, ${inputIPData.region_iso_code}`;
       }
     }
   }
@@ -20,7 +20,7 @@ export default function LocationData({data}){
   return(
     <>
     ip address
-    {/* <section>
+    <section>
       <table>
         <tr>
           <th>IP ADDRESS</th>
@@ -29,13 +29,13 @@ export default function LocationData({data}){
           <th>ISP</th>
         </tr>
         <tr>
-          <td>{data.ip_address ? data.ip_address : ''}</td>
+          <td>{inputIPData.ip_address ? inputIPData.ip_address : ''}</td>
           <td>{locationRender()}</td>
-          <td>{data ? data.timezone.name : ""}</td>
-          <td>{data ? data.connection.organization_name : ''}</td>
+          <td>{inputIPData ? inputIPData.timezone.name : ""}</td>
+          <td>{inputIPData ? inputIPData.connection.organization_name : ''}</td>
         </tr>
       </table>
-    </section> */}
+    </section>
     </>
   )
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Input from '../../components/Input/Input';
 import LocationData from '../../components/LocationData/LocationData';
 import Map from '../../components/Map/Map';
@@ -9,25 +9,9 @@ export default function Homepage(){
   const [data, setData] = useState({})
   const [inputIPData, setInputIPData] = useState({});
 
-  async function getUserIPLocationData(){
-    try{
-      const data = await apiService.getUserIPLocationData();
-      setData(data);
-      console.log(data);
-    }catch(err) {
-      console.log(err);
-      return err;
-    }
-  }
-
   function getInputIPData(data){
     setInputIPData(data);
-    console.log(data);
   }
-
-  useEffect(() => {
-    //getIPLocationData()
-  }, [])
 
   return(
     <>
