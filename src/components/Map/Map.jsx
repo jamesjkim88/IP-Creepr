ba25f96d30edb4bef986485e48e09fa6fb1bb713
James Kim <jamesjkim8806@gmail.com>
2021-10-31 20:37:16 -0700

    map rendering default location

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,22 +8,65 @@ TASKS:
 
 import React, {useState, useEffect} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import apiService from '../../utils/apiService';
 
 export default function Map(){
-  
-  return(
-    <>
-    <MapContainer center={[51.505, -0.09]} zoom={25} scrollWheelZoom={false}>
+  const [userLat, setUserLat] = useState();
+  const [userLong, setUserLong] = useState();
+
+  const cords = [51.505, -0.09];
+
+  async function getUserIPLocationData(){
+    try{
+      const data = await apiService.getUserIPLocationData();
+      setUserLat(data.latitude);
+      setUserLong(data.longitude);
+    }catch(err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  function renderMap(){
+    if(userLat && userLong){
+      return(
+        <MapContainer center={[userLat, userLong]} zoom={25} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[userLat, userLong]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
     </MapContainer>
+      )
+    }else{
+      return(
+        <div><h1>Map is loading...</h1></div>
+      )
+    }
+  }
+
+  useEffect(() => {
+    getUserIPLocationData()
+  }, [])
+  
+  return(
+    <>
+    {/* <MapContainer center={cords} zoom={25} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={cords}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer> */}
+    {renderMap()}
     </>
   )
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,38 +2,28 @@ import React, {useState, useEffect} from 'react';
 import Input from '../../components/Input/Input';
 import LocationData from '../../components/LocationData/LocationData';
 import Map from '../../components/Map/Map';
-import apiService from '../../utils/apiService';
 
 
 export default function Homepage(){
-  const [defaultUserData, setDefaultUserData] = useState({})
+  const [userData, setuserData] = useState({})
   const [inputIPData, setInputIPData] = useState({});
 
-  async function getUserIPLocationData(){
-    try{
-      const data = await apiService.getUserIPLocationData();
-      setDefaultUserData(data);
-      console.log(data);
-    }catch(err) {
-      console.log(err);
-      return err;
-    }
-  }
+
 
   function getInputIPData(data){
     setInputIPData(data);
     console.log(data);
   }
 
-  useEffect(() => {
-    //getIPLocationData()
-  }, [])
+  // useEffect(() => {
+  //   getUserIPLocationData()
+  // }, [])
 
   return(
     <>
     <Input getInputIPData={getInputIPData}/>
     <LocationData inputIPData={inputIPData}/>
-    <Map lat={inputIPData.latitude} long={inputIPData.longitude}/>
+    <Map inputIPDataLat={inputIPData.latitude} inputIPDataLong={inputIPData.longitude} userLat={userData.latitude} userLong={userData.longitude}/>
     </>
   )
 }
